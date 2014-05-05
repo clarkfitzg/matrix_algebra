b9072896d43bd0a3f0f1dd024a1119a711ef879d
Clark Fitzgerald <clarkfitzg@gmail.com>
2014-05-04 21:38:14 -0700

    determinant passes test

--- determinant.py
+++ determinant.py
@@ -11,26 +11,28 @@ def drop_ij(m, i, j):
     return np.delete(np.delete(m, i, axis=0), j, axis=1)
 
 
-
 def det(m):
     '''
     Recursive definition -- Laplace expansion
     Returns determinant of matrix m
     '''
+    if m.shape[0] != m.shape[1]:
+        raise TypeError('Must input square matrix')
+
     # Stopping condition
-    if m.ndim == 0:
-        return m
+    if m.shape == (1, 1):
+        return m[0, 0]
 
     # Use Laplace expansion along first row
     ncol = m.shape[1]
     total = 0
     for j in range(ncol):
-        total += m[0, j] * ((-1) ** (2 + j)) * det(drop_ij(m, 0, j))
+        total += m[0, j] * ((-1) ** (j)) * det(drop_ij(m, 0, j))
     
     return total
 
 
-
+############################################################
 
 m1 = np.matrix([[1, 2], [0, 3]])
 
@@ -52,3 +54,5 @@ try:
     print 'fail non-square test'
 except TypeError:
     print 'pass non-square matrix input'
+
+m10 = np.random.randn((10, 10))
